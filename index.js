347a4cc8ce42f13940d01cc4ae2477fea2af922b
PrinceSP <30646160+PrinceSP@users.noreply.github.com>
2020-07-17 16:04:14 +0800

    adding new file.

---
+++ index.js
@@ -0,0 +1,25 @@
+// let burger = document.querySelector('.burger');
+// let navigation = document.querySelector('.menus');
+//
+// burger.addEventListener('click', () => {
+//   navigation.classList.toggle('nav-on');
+//   console.log(navigation);
+// });
+
+
+let btnTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll',() => {
+  if (window.scrollY > 800) {
+    btnTop.style.display = "block";
+    btnTop.style.transition = "display 500ms linear";
+  }
+  else{
+    btnTop.style.display = "none";
+    btnTop.style.transition = "all 500ms ease-out";
+  }
+})
+
+btnTop.addEventListener('click', () => {
+  window.scrollTo(0,0);
+});
